afe2da166fdda60bc6ee20de4d275fa2228a628f
Augustin Juricic <ajuricic@neogov.net>
2016-11-11 11:56:23 +0100

    Stop using templates.

--- cuda_silentarmy/cuda_silentarmy.cpp
+++ cuda_silentarmy/cuda_silentarmy.cpp
@@ -7,14 +7,16 @@
 #include "cuda_silentarmy.hpp"
 #include "sa_cuda_context.hpp"
 
-cuda_sa_solver::cuda_sa_solver(int platf_id, int dev_id)
+cuda_sa_solver::cuda_sa_solver(int dev_id)
 {
 	device_id = dev_id;
-	getinfo(0, dev_id, m_gpu_name, m_sm_count, m_version);
+
+	int smCount;
+	getDevice(dev_id, m_gpu_name, smCount, m_version);
 
 	// todo: determine default values for various GPUs here
 	threadsperblock = 64;
-	blocks = m_sm_count * 32;
+	blocks = smCount * 32;
 }
 
 std::string cuda_sa_solver::getdevinfo()
@@ -23,14 +25,14 @@ std::string cuda_sa_solver::getdevinfo()
 		std::to_string(blocks) + ", THREADS=" + std::to_string(threadsperblock);
 }
 
-int cuda_sa_solver::getcount()
+int cuda_sa_solver::getCount()
 {
 	int device_count;
 	checkCudaErrors(cudaGetDeviceCount(&device_count));
 	return device_count;
 }
 
-void cuda_sa_solver::getinfo(int platf_id, int d_id, std::string & gpu_name, int & sm_count, std::string & version)
+void cuda_sa_solver::getDevice(int d_id, std::string & gpu_name, int & sm_count, std::string & version)
 {
 	cudaDeviceProp device_props;
 
@@ -42,24 +44,32 @@ void cuda_sa_solver::getinfo(int platf_id, int d_id, std::string & gpu_name, int
 
 }
 
-void cuda_sa_solver::start(cuda_sa_solver & device_context)
+void cuda_sa_solver::start()
 {
-	device_context.context = new sa_cuda_context(device_context.threadsperblock,
-		device_context.blocks,
-		device_context.device_id);
-
+	this->context = new sa_cuda_context(
+		threadsperblock,
+		blocks,
+		device_id);
 }
 
-void cuda_sa_solver::stop(cuda_sa_solver & device_context)
+void cuda_sa_solver::stop()
 {
-	if (device_context.context)
-		delete device_context.context;
+	if (this->context)
+		delete this->context;
 }
 
-void cuda_sa_solver::solve(const char * tequihash_header, unsigned int tequihash_header_len, const char * nonce, unsigned int nonce_len, std::function<bool()> cancelf, std::function<void(const std::vector<uint32_t>&, size_t, const unsigned char*)> solutionf, std::function<void(void)> hashdonef, cuda_sa_solver & device_context)
+void cuda_sa_solver::solve(
+	const char * header,
+	unsigned int header_len,
+	const char * nonce,
+	unsigned int nonce_len,
+	std::function<bool()> cancelf,
+	std::function<void(const std::vector<uint32_t>&, size_t, const unsigned char*)> solutionf,
+	std::function<void(void)> hashdonef)
 {
-	device_context.context->solve(tequihash_header,
-		tequihash_header_len,
+	this->context->solve(
+		header,
+		header_len,
 		nonce,
 		nonce_len,
 		cancelf,
--- cuda_silentarmy/cuda_silentarmy.hpp
+++ cuda_silentarmy/cuda_silentarmy.hpp
@@ -1,45 +1,44 @@
 #pragma once
 
+#ifdef WIN32
 #ifdef _LIB
-#define DLL_CUDA_SA __declspec(dllexport)
+#define DLL_PREFIX __declspec(dllexport)
 #else
-#define DLL_CUDA_SA
+#define DLL_PREFIX
 #endif
+#endif
+
+#include "solver/solver.h"
 
 struct sa_cuda_context;
 
-struct DLL_CUDA_SA cuda_sa_solver
+struct DLL_PREFIX cuda_sa_solver : Solver
 {
-	int threadsperblock;
-	int blocks;
+private:
+	std::string m_gpu_name;
+	std::string m_version;
 	int device_id;
 	sa_cuda_context* context;
 
-	cuda_sa_solver(int platf_id, int dev_id);
+public:
 
+	static int getCount();
+	static void getDevice(int deviceId, std::string& gpuName, int& smCount, std::string& version);
+
+	int threadsperblock;
+	int blocks;
+
+	cuda_sa_solver(int dev_id);
+	std::string getname() { return "CUDA-SILENTARMY"; }
 	std::string getdevinfo();
-
-	static int getcount();
-
-	static void getinfo(int platf_id, int d_id, std::string& gpu_name, int& sm_count, std::string& version);
-
-	static void start(cuda_sa_solver& device_context);
-
-	static void stop(cuda_sa_solver& device_context);
-
-	static void solve(const char *tequihash_header,
-		unsigned int tequihash_header_len,
+	void start();
+	void stop();
+	void solve(
+		const char *header,
+		unsigned int header_len,
 		const char* nonce,
 		unsigned int nonce_len,
 		std::function<bool()> cancelf,
 		std::function<void(const std::vector<uint32_t>&, size_t, const unsigned char*)> solutionf,
-		std::function<void(void)> hashdonef,
-		cuda_sa_solver& device_context);
-
-	std::string getname() { return "CUDA-SILENTARMY"; }
-
-private:
-	std::string m_gpu_name;
-	std::string m_version;
-	int m_sm_count;
+		std::function<void(void)> hashdonef);
 };
